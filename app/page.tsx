36fa71c6eb47f0cad156ee0f44c5a4c5ea26be25
alazaradane <alazargithub369@gmail.com>
2024-06-02 08:51:56 +0300

    adding hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+
 
 export default function Home() {
   return (
-   <main>
-      <h1 className=" font-bold text-xl">Hello Alazar</h1>
+   <main className=" relative flex items-center justify-center flex-col bg-black-100 overflow-hidden mx-auto sm:px-10 px-5">
+      <div className=" max-w-7xl w-full">
+        <Hero/>
+      </div>
    </main>
   );
 }
