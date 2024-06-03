4eb83993f3ad5ccc4d51d304785cb763d5d48262
alazaradane <alazargithub369@gmail.com>
2024-06-03 16:37:36 +0300

    "Updated page.tsx to import Grid component and added it to the main layout. Replaced next.svg with a new version and removed vercel.svg."

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
@@ -7,10 +8,11 @@ export default function Home() {
   return (
    <main className=" relative flex items-center justify-center flex-col bg-black-100 overflow-hidden mx-auto sm:px-10 px-5">
       <div className=" max-w-7xl w-full">
-        <Hero/>
         <FloatingNav navItems={[
           {name:"Home", link: "/", icon: <FaHome/>}
         ]}/>
+         <Hero/>
+         <Grid/>
       </div>
    </main>
   );
---
+++ components/Grid.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
+
+const Grid = () => {
+  return (
+    <section id='about'>
+        <BentoGrid>
+            {gridItems.map((item)=>(
+                <BentoGridItem 
+                    id={item.id}
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}/>
+            ))}
+        </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
