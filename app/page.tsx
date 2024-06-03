82e440b65fc681bb4e27aa0b5238e048009ee79f
alazaradane <alazargithub369@gmail.com>
2024-06-03 15:51:24 +0300

    "Added FloatingNav component to Home page with a single nav item 'Home'."

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 
 export default function Home() {
@@ -6,6 +8,9 @@ export default function Home() {
    <main className=" relative flex items-center justify-center flex-col bg-black-100 overflow-hidden mx-auto sm:px-10 px-5">
       <div className=" max-w-7xl w-full">
         <Hero/>
+        <FloatingNav navItems={[
+          {name:"Home", link: "/", icon: <FaHome/>}
+        ]}/>
       </div>
    </main>
   );
