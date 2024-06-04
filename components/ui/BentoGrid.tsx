7758255e466e508047a5192ed79e709d63bc7751
alazaradane <alazargithub369@gmail.com>
2024-06-04 05:14:20 +0300

    "Updated BentoGrid and BentoGridItem styles, and modified navItems data"

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,9 +10,13 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid md:auto-rows-[18rem] grid-cols-2 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
         className
       )}
+      style={{
+        background:'rgb(4,7,29)',
+        backgroundColor: 'gradient...'
+      }}
     >
       {children}
     </div>
@@ -43,7 +47,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
         className
       )}
     >
