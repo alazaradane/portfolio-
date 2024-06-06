e5da262d0c65a5246977da086d74dbf43b665948
alazaradane <alazargithub369@gmail.com>
2024-06-06 14:24:31 +0300

    "Replace img tags with next/image imports in various components"

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { InfiniteMovingCards } from './ui/InfiniteMovingCard'
 import { companies, testimonials } from '@/data'
+import Image from 'next/image'
 
 const Clients = () => {
   return (
@@ -18,8 +19,8 @@ const Clients = () => {
                 <div className=' flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
                   {companies.map(({id,img,name,nameImg})=>(
                     <div key={id} className=' flex md:max-w-60 max-w-32 gap-2 '>
-                      <img src={img} alt={name} className=' w-5 md:w-10'/>
-                      <img src={nameImg} alt={name} className=' w-20 md:w-24'/>
+                      <Image src={img} alt={name} className=' w-5 md:w-10'/>
+                      <Image src={nameImg} alt={name} className=' w-20 md:w-24'/>
                     </div>
                   ))}
                 </div>
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,7 @@
 import { workExperience } from '@/data'
 import React from 'react'
 import { Button } from './ui/MovingBorder'
+import Image from 'next/image'
 
 const Experience = () => {
   return (
@@ -18,7 +19,7 @@ const Experience = () => {
                 className=' flex-1 text-white border-neutral-200 dark:border-slate-800'
             >
                 <div className=' flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
-                    <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
+                    <Image src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
                     <div className=' lg:ms-5'>
                         <h1 className=' text-start text-xl md:text-2xl font-bold'>
                             {card.title}
