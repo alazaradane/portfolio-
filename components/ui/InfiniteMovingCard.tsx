025d64815e54410a671b0db6259875d2967e30a6
alazaradane <alazargithub369@gmail.com>
2024-06-06 13:00:27 +0300

    "Updated ESLint config and refactored InfiniteMovingCard component"

--- components/ui/InfiniteMovingCard.tsx
+++ components/ui/InfiniteMovingCard.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { cn } from "@/utils/cn";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import Image from 'next/image';
 
 export const InfiniteMovingCards = ({
   items,
@@ -14,7 +15,7 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
-    img?:string
+    img?: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -24,11 +25,7 @@ export const InfiniteMovingCards = ({
   const containerRef = React.useRef<HTMLDivElement>(null);
   const scrollerRef = React.useRef<HTMLUListElement>(null);
 
-  useEffect(() => {
-    addAnimation();
-  }, []);
-  const [start, setStart] = useState(false);
-  function addAnimation() {
+  const addAnimation = useCallback(() => {
     if (containerRef.current && scrollerRef.current) {
       const scrollerContent = Array.from(scrollerRef.current.children);
 
@@ -43,7 +40,14 @@ export const InfiniteMovingCards = ({
       getSpeed();
       setStart(true);
     }
-  }
+  }, []);
+
+  useEffect(() => {
+    addAnimation();
+  }, [addAnimation]);
+
+  const [start, setStart] = useState(false);
+
   const getDirection = () => {
     if (containerRef.current) {
       if (direction === "left") {
@@ -59,6 +63,7 @@ export const InfiniteMovingCards = ({
       }
     }
   };
+
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
@@ -70,19 +75,20 @@ export const InfiniteMovingCards = ({
       }
     }
   };
+
   return (
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20  w-screen overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20 w-screen overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
       <ul
         ref={scrollerRef}
         className={cn(
-          " flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap",
-          start && "animate-scroll ",
+          "flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap",
+          start && "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
@@ -101,19 +107,25 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm md:text-lg leading-[1.6] text-white font-normal">
+              <span className="relative z-20 text-sm md:text-lg leading-[1.6] text-white font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex gap-1">
-                  <div className=" me-3">
-                    <img className={` w-[3rem] h-[3rem] rounded-full`} src={item.img} alt="profile" />
+                  <div className="me-3">
+                    <img
+                      className="w-[3rem] h-[3rem] rounded-full"
+                      src={item.img}
+                      alt="profile"
+                      width={48}
+                      height={48}
+                    />
                   </div>
-                  <div className=" flex flex-col gap-1">
-                    <span className=" text-xl leading-[1.6] text-white font-bold">
+                  <div className="flex flex-col gap-1">
+                    <span className="text-xl leading-[1.6] text-white font-bold">
                       {item.name}
                     </span>
-                    <span className=" text-sm leading-[1.6] text-white-200 font-normal">
+                    <span className="text-sm leading-[1.6] text-white-200 font-normal">
                       {item.title}
                     </span>
                   </div>
